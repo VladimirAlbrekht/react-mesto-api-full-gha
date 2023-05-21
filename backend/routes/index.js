f8143436413fcfd31bcd9cb6517924ea7b8b8559
Vladimir Albrekht <97494847+VladimirAlbrekht@users.noreply.github.com>
2023-05-21 17:30:45 +0800

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,6 +7,15 @@ const { checkAuth } = require('../middlewares/auth');
 const { validateSignup, validateSignin } = require('../middlewares/validation');
 const { createUser, login } = require('../controllers/users');
 
+const app = express();
+
+// Тестовый маршрут, который вызывает ошибку
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 // Открытые маршруты
 router.post('/signup', validateSignup, createUser);
 router.post('/signin', validateSignin, login);
