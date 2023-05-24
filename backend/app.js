f72ee563cb01f66566c57e3cf4319252a207fe90
Vladimir Albrekht <vladimir_albreht@mail.ru>
2023-05-24 17:31:10 +0800

    Sprint 15: fixed mistakes

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,16 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config();
+const cors = require('cors');
 
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const rateLimit = require('express-rate-limit');
 const NoFoundError = require('./errors/noFoundError');
 
 const errorHandler = require('./middlewares/errorHandler');
+const corsOption = require('./middlewares/cors');
 const rootRouter = require('./routes/index');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -30,6 +34,23 @@ mongoose.connect('mongodb://localhost:27017/mestobd', { useNewUrlParser: true, u
 // Подключаем логгер запросов
 app.use(requestLogger);
 
+// cors запросы
+app.use(cors(corsOption));
+
+// краш-тест сервера
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+// Ограничение запросов
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 минут
+  max: 100, // не более 100 запросов за 15 минут
+});
+
+// Применяем лимитер для всех маршрутов
+app.use(limiter);
 // Подключаем корневой роутер
 app.use(rootRouter);
 
@@ -53,4 +74,3 @@ app.use(errorHandler);
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
-
