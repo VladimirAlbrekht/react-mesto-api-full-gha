ece1b3eb61b66ad965e11aa7e3cefdda15f38a69
Vladimir Albrekht <vladimir_albreht@mail.ru>
2023-05-26 10:22:45 +0800

    15-fixed mistakes

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 
 require('dotenv').config();
 
-const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
