0778f6161cc2822165caf7be81c905a236b24f62
Vladimir Albrekht <vladimir_albreht@mail.ru>
2023-05-27 20:32:10 +0800

    15

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -14,11 +14,10 @@ const getCards = (req, res, next) => {
 
 const createCard = (req, res, next) => {
   const { name, link } = req.body;
-  const owner = req.user._id;
 
-  Card.create({ name, link, owner })
+  Card.create({ name, link, owner: req.user._id })
     .populate(['owner', 'likes'])
-    .then((card) => res.json(card))
+    .then((card) => res.send({ data: card }))
     .catch((error) => {
       if (error instanceof ValidationError) {
         return next(new ValidationError(error.message));
